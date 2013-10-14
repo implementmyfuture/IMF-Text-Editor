762dce004b680a7fa8340e302f779d613b137434
Implement My Future <ImplementMyFuture@gmail.com>
2013-10-13 21:08:01 -0400

    initial commit

---
+++ src/view/TextEditorFrame.java
@@ -0,0 +1,25 @@
+package view;
+
+import javax.swing.JFrame;
+
+/**
+ *  This class is the main window of the text editor application
+ * @author Robert
+ * 
+ */
+public class TextEditorFrame extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public TextEditorFrame (){
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
